896239240329dbe8c4f64f43c35c47cf6893a745
Eugene Rebedailo <mokonamodokidesu@gmail.com>
2020-03-09 00:25:04 +0300

    Fixed random test

--- test/index.ts
+++ test/index.ts
@@ -3,6 +3,14 @@ import solve from "../src/index";
 import "lodash.combinations";
 import * as _ from "lodash";
 
+function* makePairIterator(inputs: number[]) {
+    for (let i = 0; i < inputs.length; i++) {
+        for (let j = 0; j < inputs.length; j++) {
+            yield [inputs[i], inputs[j]];
+        }
+    }
+}
+
 describe("Funny math", () => {
     it("Should have a solve() function", () => {
         expect(solve).to.be.a('function');
@@ -21,14 +29,11 @@ describe("Funny math", () => {
     it("Should pass a random test several times", () => {
         _.times(10, () => {
             const n = _.random(5, 10);
-            const expectedOutput = _.times(n, () => _.random(1, 500, false))
-            const allPairs = (_ as any).combinations(expectedOutput, 2) as number[][];
-            const inputs = _.chain(allPairs)
-                .map(_.sum)
-                .sort()
-                .value();
+            const expectedOutput = _.times(n, () => _.random(1, 500, false));
+            const allPairs = Array.from(makePairIterator(expectedOutput));
+            const inputs = _.chain(allPairs).map(_.sum).value().sort();
             const actualResult = solve(n, inputs);
-            const sortedResult = _.sortBy(actualResult, _.identity)
+            const sortedResult = _.sortBy(actualResult, _.identity);
             expect(sortedResult).to.deep.equal(expectedOutput);
         });
     });
